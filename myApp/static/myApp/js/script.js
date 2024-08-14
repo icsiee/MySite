d7da0d39248afe76a12aa0875fcc7a41909c5f70
icsiee <iclaldr@gmail.com>
2024-08-14 16:02:05 +0300

    Draft completed.

--- myApp/static/myApp/js/script.js
+++ myApp/static/myApp/js/script.js
@@ -1,17 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const themeToggle = document.getElementById('toggle-theme');
+    const themeToggle = document.getElementById('theme-toggle');
     const header = document.querySelector('header');
 
-    if (themeToggle) { // themeToggle'un var olup olmadığını kontrol ediyoruz
+    if (themeToggle) {
         themeToggle.addEventListener('click', function() {
             if (document.body.classList.contains('light-theme')) {
                 document.body.classList.remove('light-theme');
                 document.body.classList.add('dark-theme');
                 header.style.backgroundImage = "url('static/myApp/images/resim_2024-08-12_143218113.jpg')";
+                stylesheet.setAttribute('href', 'static/myApp/css/dark-theme.css');
             } else {
                 document.body.classList.remove('dark-theme');
                 document.body.classList.add('light-theme');
                 header.style.backgroundImage = "url('static/myApp/images/night-sky-colorful-3840x2160-12510.jpg')";
+                stylesheet.setAttribute('href', 'static/myApp/css/light-theme.css');
             }
         });
     } else {
@@ -25,12 +27,13 @@ document.addEventListener('DOMContentLoaded', function() {
         .then(response => response.json())
         .then(data => {
             projectsContainer.innerHTML = data.map(repo => {
+                // Tarih formatlama
                 const createdDate = new Date(repo.created_at).toLocaleDateString();
                 return `
                     <div class="project-card">
                         <h3><a href="${repo.html_url}" target="_blank">${repo.name}</a></h3>
                         <p>Created: ${createdDate}</p>
-                        <p>Stars: ${repo.stargazers_count}</p>
+                        <!-- Yıldız bilgisi kaldırıldı -->
                     </div>
                 `;
             }).join('');
